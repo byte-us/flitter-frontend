1a5354f92ed641a7efa532eef973b55d910a2f4d
Andrea Aparicio <apariciolopez.andrea@gmail.com>
2023-02-04 02:39:03 +0100

    Store interface de Posts hecho.

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,11 +1,13 @@
 <template>
   <nav class="bar">
       <div class="logo">
-        <router-link to="/"
-          ><img alt="Flitter logo" src="../assets/logo.png" width="50" />
+        <router-link to="/">
+          <img alt="Flitter logo" src="../assets/logo.png" width="50" />
         </router-link>
       </div>
-      <div class="title"><h1>Flitter</h1></div>
+      <div class="title">
+        <h1>Flitter</h1>
+      </div>
     <div class="links">
       <router-link to="/login">Log in</router-link> |
       <router-link to="/singup">Sign up</router-link>
@@ -21,14 +23,32 @@ export default defineComponent({});
 
 <style scoped>
 .bar {
-  display: flex;
-  justify-content: space-between;
+  /* display: flex;
+  justify-content: space-between; */
+  display: 	grid;
   align-items: center;
+  grid-template-columns: 1fr 1fr 1fr;
   padding: 10px;
+}
+
+.logo {
+  justify-self: left;
+}
+
+.title {
+  justify-self: center;
 }
 
 h1 {
   color: purple;
   font-family: "Righteous", cursive;
+}
+
+.links {
+  justify-self: right;
+}
+
+a {
+  text-decoration: none;
 }
 </style>
